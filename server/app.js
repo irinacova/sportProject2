11b02a050890edc7cd494e28e7f5b58b5630d129
irinacova <kovalenko.23@inbox.ru>
2024-07-02 13:07:23 +0300

    bd

--- server/app.js
+++ server/app.js
@@ -1,24 +1,32 @@
+// подключаем экспресс
 const express = require('express');
+//подключаем пути, можем загружать статичные файлы
 const path = require('path')
+//подключаем енв для кодирования паролей и портаб в данном случае для порта
+require('dotenv').config();
+//подключаем приложение 
 const app = express();
+//миддлварка на удаление заголовка в запросе в инструментах разработика
 const removeHeader = require('./middleware/removeHeaders')
+//находит пользователя и записывает его в апплокалс
 const userFind = require('./middleware/userFind')
+//позволяет читать пост запросы
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//для чтения статичных файлов
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//используем функции миддлварки
 app.use(removeHeader);
 app.use(userFind);
-
+//подключааем роуты
 const indexRouter = require('./routes/index')
 app.use('/api',indexRouter)
 
 
 
-
-app.listen(3000, () => { console.log('Server started on 3000 port') });
+//запускаем сервер 
+app.listen(process.env.PORT, () => { console.log(`Server started on ${process.env.PORT} port`) });
 
 
 
